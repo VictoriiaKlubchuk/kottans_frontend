4371a252913683dbc3a392a9a1946051ba1cf153
VictoriiaKlubchuk <victoriia.klubchuk@gmail.com>
2018-02-07 15:47:39 +0200

    Draft for weather app

---
+++ task_10/gulpfile.js
@@ -0,0 +1,75 @@
+var gulp         = require('gulp'),
+	sass         = require('gulp-sass'),
+	browserSync  = require('browser-sync'),
+	imagemin     = require('gulp-imagemin'),
+	pngquant     = require('imagemin-pngquant'),
+	cache        = require('gulp-cache'),
+	autoprefixer = require('gulp-autoprefixer'),
+	concat       = require('gulp-concat'),
+	copy         = require('gulp-copy');
+	sourcemaps   = require('gulp-sourcemaps');
+
+gulp.task('sass', function() {
+	return gulp.src('src/scss/**/*.scss')
+	.pipe(sass())
+	.pipe(gulp.dest('src/css'))
+	.pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('css', function() {
+	return gulp.src('src/css/*.css')
+	.pipe(autoprefixer({
+	browsers: ['last 15 versions','>1%', 'ie 8', 'ie 7'],
+	cascade: false
+	}))
+	.pipe(gulp.dest('dist/css'))
+});
+
+gulp.task('browserSync', () => {
+	browserSync({
+		server: {
+			baseDir: 'dist'
+		},
+		notify: false
+	});
+});
+
+gulp.task('pages', function() {
+	return gulp.src('src/*.html')
+	.pipe(gulp.dest('dist'))
+	.pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('img', () => {
+	gulp.src('src/img/*')
+	.pipe(imagemin({
+	    interlaced: true,
+	    progressive: true,
+	    optimizationLevel: 5,
+	    svgoPlugins: [{removeViewBox: true}]
+	}))
+	.pipe(gulp.dest('dist/img'))
+});
+
+gulp.task('copy', ()=>{
+	return gulp.src('src/fonts/*.*')
+	.pipe(gulp.dest('dist/fonts'))
+	.pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('scripts', () => {
+	return gulp.src('src/js/*.js')
+	.pipe(sourcemaps.init({loadMaps: true}))
+	.pipe(concat('script.js'))
+	.pipe(sourcemaps.write('../maps'))
+	.pipe(gulp.dest('dist/js'))
+	.pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('default', function() {
+	gulp.start('pages', 'sass','scripts', 'img', 'browserSync', 'css', 'copy');
+	gulp.watch('src/*.html', ['pages']);
+	gulp.watch('src/scss/**/*.scss', ['sass']);
+	gulp.watch('src/css/*.css', ['css']);
+	gulp.watch('src/js/*.js', ['scripts']);
+});
---
+++ task_10/src/js/script1.js
@@ -0,0 +1,51 @@
+'use strict';
+
+// get some info from 
+let input = document.getElementById('userCity');
+let submit = document.getElementById('submit');
+submit.addEventListener('click', getValue);
+
+function getValue() {
+	// city name by default "London"
+	// In this part of code I got full name of URL
+	// Now it is just with Celsium not Farengate
+
+	// our container
+	var div = document.getElementById('weather');
+
+	// use api openweather.org to compile URL + some parametrs
+	var city = input.value;
+	var api = 'http://api.openweathermap.org/data/2.5/weather?q=';
+	var apiKey = '&APPID=03cfb0fcf2a70f32f32cfbb7dd39a3ec';
+	let units = '&units=metric';
+
+	// full url for request
+	let fullUrl = api + city + units  + apiKey;
+
+	// use fetch for getting respond from API
+	// with city form Input
+	let promise = fetch(fullUrl, {type:'cors'});
+
+	promise
+		.then((response)=>{
+			return response.json();
+			}, (err)=> {
+				console.log(err);
+				throw err;
+			})
+		.then((json)=>{
+			let data = json;
+
+			// print data to HTML
+			div.innerHTML = 
+			`
+			<div>
+			<p>city name: ${ data.name }</p>
+			<p>avarage temp: ${ data.main.temp } </p>
+			<p> max_temp: ${ data.main.temp_max} ... min temp: ${data.main.temp_min}</p>
+			</div>
+			`
+			console.log(data);
+		})
+}
+
