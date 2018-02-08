ac0bad5eb53b00eb98b28565df02e9ff56bfb9bb
VictoriiaKlubchuk <victoriia.klubchuk@gmail.com>
2018-02-08 12:42:59 +0200

    add new function in js, delete script1/2.js

--- task_10/src/js/script1.js
+++ task_10/src/js/script1.js
@@ -1,29 +1,29 @@
 'use strict';
 
-// get some info from 
 let input = document.getElementById('userCity');
+
 let submit = document.getElementById('submit');
-submit.addEventListener('click', getValue);
+submit.addEventListener('click', userInputController);
 
-function getValue() {
+function userInputController() {
 	// city name by default "London"
-	// In this part of code I got full name of URL
-	// Now it is just with Celsium not Farengate
-
-	// our container
 	var div = document.getElementById('weather');
-
-	// use api openweather.org to compile URL + some parametrs
 	var city = input.value;
-	var api = 'https://api.openweathermap.org/data/2.5/weather?q=';
-	var apiKey = '&APPID=03cfb0fcf2a70f32f32cfbb7dd39a3ec';
+	console.log(city);
+	weatherService(city, div);
+}
+
+// @param {cityName} - input value
+// @param {elem} - in that case div#weather
+function weatherService(cityName, elem) {
+	let api = 'https://api.openweathermap.org/data/2.5/weather?q=';
+	let apiKey = '&APPID=03cfb0fcf2a70f32f32cfbb7dd39a3ec';
 	let units = '&units=metric';
 
 	// full url for request
-	let fullUrl = api + city + units  + apiKey;
+	let fullUrl = api + cityName + units  + apiKey;
+	console.log(fullUrl);
 
-	// use fetch for getting respond from API
-	// with city form Input
 	let promise = fetch(fullUrl, {type:'cors'});
 
 	promise
@@ -32,20 +32,23 @@ function getValue() {
 			}, (err)=> {
 				console.log(err);
 				throw err;
-			})
+		})
 		.then((json)=>{
 			let data = json;
-
 			// print data to HTML
-			div.innerHTML = 
-			`
-			<div>
-			<p>city name: ${ data.name }</p>
-			<p>avarage temp: ${ data.main.temp } </p>
-			<p> max_temp: ${ data.main.temp_max} ... min temp: ${data.main.temp_min}</p>
-			</div>
-			`
+			renderHTML(elem, data)
 			console.log(data);
 		})
 }
 
+// this function is used inside promise.then
+function renderHTML(elem, data) {
+	elem.innerHTML = 
+		`
+		<div>
+			<p>city name: ${ data.name }</p>
+			<p>avarage temp: ${ data.main.temp } </p>
+			<p> max_temp: ${ data.main.temp_max} ... min_temp: ${data.main.temp_min}</p>
+		</div>
+		`
+}
